8e4a2d24502eaabd4f22293dd9620085cab17d16
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-09 12:05:02 +0200

    [UPDATE]: file script

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,13 +22,16 @@ Creare due variabili con il valore dello sconto, uno per i minorenni, uno per gl
 
 // 1.
 // dichiarazione dei riferimenti
-const firstNameRef = document.getElementById("firstname");
+const firstNameRef = document.getElementById("firstName");
 const distanceRef = document.getElementById("distance");
 const userAgeRef = document.getElementById("userAge");
 const buttonGenRef = document.getElementById("btn-1");
 const buttonResRef = document.getElementById("btn-2");
+const passengerRef = document.getElementById('passenger');
+const offerRef = document.getElementById('offer');
+const carriageRef = document.getElementById('carriage');
+const cpCodeRef = document.getElementById('cpCode');
 const ticketPriceRef = document.getElementById("ticketPrice");
-
 // const kmTravel = 100; //parseInt(prompt("Quanti chilometri desideri percorrere?"));
 // const age = 20; //parseInt(prompt("Quanti anni hai?"));
 
@@ -38,6 +41,7 @@ const ticketPriceRef = document.getElementById("ticketPrice");
 const tariff = 0.21;
 const discountUnderAge = 20;
 const discountOverAge = 40;
+let message;
 
 // 3.
 // evento onclick del pulsante genera
@@ -49,11 +53,17 @@ buttonGenRef.addEventListener("click", function () {
     let ticketPrice = distance * tariff;
     if (userAge === "min") {
       ticketPrice *= 1 - discountUnderAge / 100;
+      message = 'Biglietto scontato al 20%';
     } else if (userAge === "anz") {
       ticketPrice *= 1 - discountOverAge / 100;
+      message = 'Biglietto scontato al 40%';
+    }else{
+      message = 'Biglietto standard';
     }
-    debugger;
+    offerRef.innerHTML = message;
+    passengerRef.innerHTML = firstNameRef.value;
     ticketPriceRef.innerHTML = ticketPrice.toFixed(2).toString() + " €";
+
   }
 });
 
